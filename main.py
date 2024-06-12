92df1582103700fbe255693ff53f128dfa63f75b
darkhacker34 <72502160+darkhacker34@users.noreply.github.com>
2024-06-12 17:59:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 
+from pyrogram import Client, filters
+
+AI="1917094"
+AH="43dbeb43f27f99752b44db7493bf38ad"
+BT="6941473830:AAFlRLUUhjPIfrYWOeBjVrxMbygS0VX4N7w"
+
+NIK = Client (
+  name="dark",
+  api_id=AI,
+  api_hash=AH,
+  bot_token=BT,
+  
+)
+
+print("Bot started!")
+
+NIK.run()
