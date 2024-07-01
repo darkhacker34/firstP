769eea7408896731143708a517ad251c403faf05
darkhacker34 <72502160+darkhacker34@users.noreply.github.com>
2024-07-01 18:58:29 +0530

    Update and rename main.py to forward.py

--- forward.py
+++ forward.py
@@ -1,9 +1,6 @@
 
 from pyrogram import Client, filters
-
-AI="1917094"
-AH="43dbeb43f27f99752b44db7493bf38ad"
-BT="6941473830:AAFlRLUUhjPIfrYWOeBjVrxMbygS0VX4N7w"
+import os
 
 NIK = Client (
   name="dark",
