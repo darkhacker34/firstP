382b83c75e0a45aaaa2c83097d239c99ac2de912
darkhacker34 <72502160+darkhacker34@users.noreply.github.com>
2024-07-02 14:44:02 +0530

    Update forward.py

--- forward.py
+++ forward.py
@@ -2,17 +2,15 @@
 from pyrogram import Client, filters
 from dotenv import load_dotenv, dotenv_values
 import os
-from os import environ
 import asyncio
-from webcode import bot_run
-from aiohttp import web as webserver
 
 config = load_dotenv("config.env")
 
 api_id = os.getenv('API_ID', '')
 api_hash = os.getenv('API_HASH', '')
 bot_token = os.getenv('BOT_TOKEN', '')
-PORT_CODE = os.getenv("PORT", "8080")
+
+print("Bot Logging...")
 
 bot = Client(
   "dark",
@@ -20,20 +18,17 @@ bot = Client(
   api_hash=api_hash,
   bot_token=bot_token,
   )
-  
+  print("Bot login successfully")
+
 @bot.on_message(filters.command("start") & filters.private)
 async def start_msg(client, msg):
+  await print("start command")
   user_men = msg.from_user.mention
   await msg.reply_text(f"Helo {user_men}")
   
 @bot.on_message(filters.channel & filters.media & filters.text & filters.photo & filters.incoming)
 async def incoming(client, msg):
   await msg.forward("-1001613693052")
-
-client = webserver.AppRunner(bot_run())
-client.setup()
-bind_address = "0.0.0.0"
-webserver.TCPSite(client, bind_address, PORT_CODE).start()
-
+  
 print("Bot started!")
 bot.run()
